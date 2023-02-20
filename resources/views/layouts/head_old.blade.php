b672ecfff0ae730089f2cfd2d3429d33a55d0155
muruganeauditor <murugan@eauditoroffice.com>
2023-02-20 17:14:41 +0530

    fix header

--- resources/views/layouts/head_old.blade.php
+++ resources/views/layouts/head_old.blade.php
@@ -25,6 +25,7 @@
     <link href="{{ url('assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
     <!-- <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> -->
     <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ url('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
     <link href="{{ url('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
     <!-- Template Main CSS File -->
